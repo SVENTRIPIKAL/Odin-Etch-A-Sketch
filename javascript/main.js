3307940d0b8a4195d0e9f59f4a898d1c7ae310c2
SVENTRIPIKAL <180786857+SVENTRIPIKAL@users.noreply.github.com>
2025-09-25 21:31:44 +0000

    Add javascript & CSS -added function to create square div elements -added function to create container div elements containing 16 squares -modified CSS to style squares & containers

---
+++ javascript/main.js
@@ -0,0 +1,23 @@
+// returns square class div element
+function getSquareDiv() {
+    let squareDiv = document.createElement("div")
+    squareDiv.setAttribute("class", "square")
+    return squareDiv
+}
+
+// returns container class div element containing 16 squares
+function get16SquareContainer() {
+  let squareList = Array.apply(null, Array(16)).map(() => getSquareDiv())
+  
+  let containerDiv = document.createElement("div")
+  containerDiv.setAttribute("class", "container")
+  containerDiv.append(...squareList)
+  
+  return containerDiv
+}
+
+// stack containers along the body element box axis
+for (let i = 0; i < 16; i++) {
+  let container = get16SquareContainer()
+  document.body.append(container)
+}
