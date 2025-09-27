9f33a1d27b02a9f8d1ccd299902186b1263c8d14
SVENTRIPIKAL <180786857+SVENTRIPIKAL@users.noreply.github.com>
2025-09-27 05:28:51 +0000

    Add gridReset & square height function -updated functions to size squares according to squares per container -added button to reset grid to user-input grid size -added CSS declarations for button

--- javascript/main.js
+++ javascript/main.js
@@ -1,33 +1,60 @@
-// sets square color to red else grey
+// sets square color to Red else Grey
 function colorSquare(event) {
     let square = event.target.closest(".square")
     switch (square.style.backgroundColor) {
-    case "red": { square.style.backgroundColor = "grey" }; break;
-    default:  { square.style.backgroundColor = "red" }; break;
+        case "red": { square.style.backgroundColor = "grey" }; break;
+        default:  { square.style.backgroundColor = "red" }; break;
     }
 }
 
-// returns square class div element with mouseover event listener
-function getSquareDiv() {
+// returns an approximate height to be distributed across 960px
+function getSquareHeight(n) {
+    return Math.floor(960 / n)
+}
+
+// returns square class div element with height of N & mouseover event listener
+function getSquareDiv(n) {
     let squareDiv = document.createElement("div")
     squareDiv.setAttribute("class", "square")
+    squareDiv.style.height = `${getSquareHeight(n)}px`
     squareDiv.addEventListener("mouseover", (event) => colorSquare(event))
     return squareDiv
 }
 
-// returns container class div element containing 16 squares
-function get16SquareContainer() {
-    let squareList = Array.apply(null, Array(16)).map(() => getSquareDiv())
-
+// returns container class div element containing N squares
+function getSquareContainer(n) {
+    let squareList = Array.apply(null, Array(n)).map(() => getSquareDiv(n))
     let containerDiv = document.createElement("div")
     containerDiv.setAttribute("class", "container")
     containerDiv.append(...squareList)
-
     return containerDiv
 }
 
-// stack containers along the body element box axis
-for (let i = 0; i < 16; i++) {
-    let container = get16SquareContainer()
-    document.body.append(container)
+// stacks a list of N containers along the body element box axis
+function createGrid(n) {
+    let containerList = Array.apply(null, Array(n))
+                             .map(() => getSquareContainer(n))
+    document.body.append(...containerList)
 }
+
+// displays popup to receive new grid size via user input
+function newGridSizePrompt() {
+    let input = prompt("Enter New Grid Size (Max: 100): ")
+}
+
+// creates gridReset class button with click event listener
+function createGridResetButton() {
+    let button = document.createElement("button")
+    button.setAttribute("class", "gridReset")
+    button.textContent = "RESET GRID"
+    button.addEventListener("click", () => newGridSizePrompt())
+    document.body.append(button)
+}
+
+
+
+// create grid reset button
+createGridResetButton()
+
+// create main default size grid
+createGrid(16)
